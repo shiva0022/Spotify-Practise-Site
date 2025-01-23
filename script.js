7f98b47a8a41657ebbbce87f14aed941fdab6e52
Shiva <shivajakka8@gmail.com>
2025-01-23 21:43:27 +0530

    albums loader added

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+var folders = []
+
+async function loadAlbums() {
+    let x = await fetch('http://127.0.0.1:3000/assets/songs/');
+    let response = await x.text();
+    let div = document.createElement('div');
+    div.innerHTML = response;
+    let links = div.getElementsByTagName('a')
+    Array.from(links).forEach(element => {
+        if(element.href.includes('/songs/')) {
+            let folderLink = element.href;
+            folders.push(folderLink.match(/\/songs\/(.*)/)[1]);
+        }
+    });
+    let albumContainer = document.querySelector('.album-container');
+    for (folder of folders) {
+        let info = await fetch(`http://127.0.0.1:3000/assets/songs/${folder}/info.json`);
+        info = await info.json();
+        albumContainer.innerHTML = albumContainer.innerHTML + `
+            <div class="card" data-folder="${folder}">
+                <img src="./assets/songs/${folder}/card_img.jpg" alt="album-cover">
+                <h2>${info.title}</h2>
+                <div class="content d-flex">
+                    <p>Artist</p>
+                    <li>${info.artist}</li>
+                </div>
+                <button type="button">
+                    <img src="./assets/icons/play.svg" alt="" width="20px">
+                </button>
+            </div>
+        `;
+    }
+}
+
 async function main() {
-    
+    loadAlbums();
 }
 
 main()
