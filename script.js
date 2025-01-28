e7dc216ed93b4841125aabbbd1f4bc895a6478cd
Shiva <shivajakka8@gmail.com>
2025-01-28 12:43:05 +0530

    hosting changes

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var folders = []
 
 async function loadAlbums() {
-    let x = await fetch('http://127.0.0.1:3000/assets/songs/');
+    // let x = await fetch('http://127.0.0.1:3000/assets/songs/');
+    // let x = await fetch('https://github.com/shiva0022/Spotify-Practise-Site/tree/main/assets/songs/');
+    let x = await fetch('https://shiva0022.github.io/Spotify-Practise-Site/assets/songs/');
     let response = await x.text();
     let div = document.createElement('div');
     div.innerHTML = response;
@@ -14,9 +16,10 @@ async function loadAlbums() {
     });
     let albumContainer = document.querySelector('.album-container');
     for (folder of folders) {
-        let info = await fetch(`http://127.0.0.1:3000/assets/songs/${folder}/info.json`);
+        // let info = await fetch(`http://127.0.0.1:3000/assets/songs/${folder}/info.json`);
+        let info = await fetch(`https://github.com/shiva0022/Spotify-Practise-Site/tree/main/assets/songs/${folder}/info.json`);
         info = await info.json();
-        albumContainer.innerHTML = albumContainer.innerHTML + `
+        albumContafiner.innerHTML = albumContainer.innerHTML + `
             <div class="card" data-folder="${folder}">
                 <img src="./assets/songs/${folder}/card_img.jpg" alt="album-cover">
                 <h2>${info.title}</h2>
